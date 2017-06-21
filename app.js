bae58adf83b1303cbb8ad244804a51c328889081
Gildon Kirk <gildonkirk@gmail.com>
2017-06-21 13:22:01 -0400

    firebase connection working

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-var header = $('header').html();
-$(document).ready(function(){
-	console.log(header);
+
+var database = firebase.database();
+
+$('#enter').on("click", function() {
+	event.preventDefault();
+	var name = $('#name-input').val();
+	var wins = 0;
+	var losses = 0;
+
+	database.ref().set({
+		username: name,
+		userWins: wins,
+		userLosses: losses
+	});
+});
+	
+database.ref().on('value', function(snapshot) {
+	$('#player1').text(snapshot.val().username);
 });
