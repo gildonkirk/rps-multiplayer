4d09c97396e7c44dca98d877c84c16a497a8102c
Gildon Kirk <gildonkirk@gmail.com>
2017-06-26 01:33:56 -0400

    turns work now, js for border css needs work

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ $('#enter').on("click", function() {
 	event.preventDefault();
 	var name = $('#name-input').val();
 	var playerChoice = $('<p>').addClass('playerChoice');
-	$('form').remove();
 	if (player1 === false){
 		database.ref('players/one').set({
 			username: name,
@@ -18,11 +17,11 @@ $('#enter').on("click", function() {
 			userLosses: losses
 		});
 
-		// telling what player you are not working
-		$('#gameInstruct').text('You are player 1')
+		$('header').after('<p>You are player 1</p>')
 		$('#player1').append('<div class="rps rps1">Rock</div>');
 		$('#player1').append('<div class="rps rps1">Paper</div>');
 		$('#player1').append('<div class="rps rps1">Scissors</div>');
+		$('form').remove();
 	} else {
 		database.ref('players/two').set({
 			username: name,
@@ -32,14 +31,14 @@ $('#enter').on("click", function() {
 		database.ref('turn').set({
 			turn: turnCount
 		});	
-		console.log(turnCount);
-		// telling what player tou are not working
-		$('#gameInstruct').text('You are player 2')		
+		$('header').after('<p>You are player 2</p>')		
 		$('#player2').append('<div class="rps rps2">Rock</div>');
 		$('#player2').append('<div class="rps rps2">Paper</div>');
 		$('#player2').append('<div class="rps rps2">Scissors</div>');
+		$('form').remove();
 	}	
 });
+
 
 
 //when firebase is updated, displays usernames on each users screen
@@ -56,20 +55,19 @@ database.ref('players').on('child_added', function(snapshot) {
 
 // sends choice to firebase and displays it only to local user, should toggle between turns
 database.ref('turn/turn').on('value', function(snapshot) {
-	console.log(snapshot.val());
-	// not currently working to switch from one persons turn to the other
 	if ((snapshot.val()) % 2 != 0) {
+		$('#player1').css('border', '2px solid yellow');
+		$('#player2').css('border', '2px solid #222222');
 		$(document).on('click', '.rps1', rps1choice)
-		console.log('player1test');
+		.off('click', '.rps2', rps2choice);
 	} else {
+		$('#player2').css('border', '2px solid yellow');
+		$('#player1').css('border', '2px solid #222222');
 		$(document).on('click', '.rps2', rps2choice)
-		console.log('player2test');
+		.off('click', '.rps1', rps1choice);
 	}
 });
 
-
-
-// not able to make 1st player choice right now
 function rps1choice() {
 	console.log('player1');
 	var rpsChoice = $(this).text();
@@ -80,7 +78,9 @@ function rps1choice() {
 		choice: rpsChoice,
 	});
 	turnCount++;
-	console.log(turnCount);
+	database.ref('turn').set({
+		turn: turnCount
+	});	
 };
 
 function rps2choice() {
@@ -93,8 +93,12 @@ function rps2choice() {
 		choice: rpsChoice
 	});
 	turnCount++;
-	console.log(turnCount);
+	database.ref('turn').set({
+		turn: turnCount
+	});
 };
+
+
 
 // Need to make it so reload only deletes that users name, wins, etc. right now it gets rid of both users
 $(window).on('unload', function(){
